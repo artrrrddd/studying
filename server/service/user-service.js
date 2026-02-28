b388497813858d69671488718f48d15dd697d426
artur <bataev2arthur@gmail.com>
2026-02-28 18:08:26 +0300

    'uuid'

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,6 +1,5 @@
 const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
-const uuid = require('uuid');
 const mailService = require('./mail-service');
 const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
@@ -8,12 +7,13 @@ const ApiError = require('../exceptions/api-error');
 
 class UserService {
 async registration(email, password) {
+    const { v4: uuidV4 } = await import('uuid');
     const candidate = await UserModel.findOne({email})
     if (candidate) {
         throw ApiError.BadRequest(`Пользователь с почтовым адресом ${email} уже существует`)
     }
     const hashPassword = await bcrypt.hash(password, 3);
-    const activationLink = uuid.v4();
+    const activationLink = uuidV4();
     const user = await UserModel.create({email, password: hashPassword, activationLink});
     
     // Email отправка отключена (настройте SMTP в .env для включения)
