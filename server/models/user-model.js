997be912ad83bc12772edd820af571143bfb37e7
artur <bataev2arthur@gmail.com>
2026-02-28 20:41:15 +0300

    ''

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,12 +1,12 @@
-const {Schema, model} = require('mongoose');
+const mongoose = require('mongoose')
 
-const UserSchema = new Schema ({
-     email: {type: String, unique: true, required: true},
-     password: {type: String, required: true},
-     isActivated: {type: Boolean, default: false},
-     activationLink: {type: String},
+const UserSchema = new mongoose.Schema({
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  isActivated: { type: Boolean, default: false },
+  activationLink: { type: String },
 })
 
-module.exports = model('User', UserSchema)
-
-
+module.exports =
+  mongoose.models.User ||
+  mongoose.model('User', UserSchema)
