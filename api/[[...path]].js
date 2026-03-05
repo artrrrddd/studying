f80b62c58e679688540489124181c5002cd079c5
artur <bataev2arthur@gmail.com>
2026-03-05 20:48:24 +0300

    ''

--- api/[[...path]].js
+++ api/[[...path]].js
@@ -1,12 +1,18 @@
 // api/index.js
-const serverless = require('serverless-http')
+
 const createApp = require('../server/app')
 
 let cachedApp = null
 
-module.exports = serverless(async () => {
-  if (!cachedApp) {
-    cachedApp = await createApp()
+module.exports = async (req, res) => {
+  try {
+    if (!cachedApp) {
+      cachedApp = await createApp()
+    }
+    return cachedApp(req, res)
+  } catch (err) {
+    console.error('🔥 App initialization error:', err)
+    res.statusCode = 500
+    res.end('Internal server error')
   }
-  return cachedApp
-})
+}
