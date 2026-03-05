26447f9e353d2e3a1d80ef53888d90032dad62c9
artur <bataev2arthur@gmail.com>
2026-03-05 20:21:27 +0300

    ''

--- client/src/components/Lessons.jsx
+++ client/src/components/Lessons.jsx
@@ -7,23 +7,27 @@ import { Link } from "react-router-dom"
 
 const Lessons = () => {
 
-    const lessons = useSelector(s => s.lessons.items)
+    const lessons = useSelector(s => s.lessons.myItems) || []
 
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(fetchLessonsThunk())
-    }, [])
+    }, [dispatch])
 
     return (
-        <>
         <div>
-            {lessons.map(e => <div>{
-            <Link to={`/lessons/${e.id}`}>
-            {e.title}
-            </Link>
-            }</div>)}    
+            {lessons.length > 0 ? (
+                lessons.map(e => (
+                    <div key={e.id}>
+                        <Link to={`/lessons/${e.id}`}>
+                            {e.title}
+                        </Link>
+                    </div>
+                ))
+            ) : (
+                <div>Уроков пока нет</div>
+            )}
         </div>
-        </>
     )
 }
 
